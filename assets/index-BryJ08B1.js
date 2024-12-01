5309b225a2e45fee65c5f2a08d7de3a71d26605a
Nikita Kisiura <nikitar156@gmail.com>
2024-12-01 12:11:03 -0500

    Updates

---
+++ assets/index-BryJ08B1.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-klYm2mnE.js";const e=()=>(console.log("not found page loaded"),o.jsx("div",{children:"Page not found "}));export{e as Component};
---
+++ assets/index-BzGtfSio.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-klYm2mnE.js";const r=()=>(console.log("character details page loaded"),e.jsx("div",{children:"character details page"}));export{r as Component};
