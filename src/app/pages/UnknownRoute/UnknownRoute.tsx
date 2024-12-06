2bf211071e01d757ba2856b0788890b0c29de2b4
Nikita Kisiura <nikitar156@gmail.com>
2024-12-06 18:45:33 -0500

    style AppPageLoader & UnknownRoute pages

--- src/app/pages/UnknownRoute/UnknownRoute.tsx
+++ src/app/pages/UnknownRoute/UnknownRoute.tsx
@@ -1,3 +1,28 @@
+import { useNavigate } from "react-router";
+import { Button } from "@nextui-org/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+
 export const UnknownRoute = () => {
-  return <div>Page not found </div>;
+  const navigate = useNavigate();
+
+  const handleBackToHomeClick = () => {
+    void navigate("/home");
+  };
+
+  return (
+    <div className="container py-4">
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-center text-2xl">Oops... Page not found</p>
+        <Button
+          color="success"
+          variant="ghost"
+          startContent={<FontAwesomeIcon icon={faArrowLeftLong} />}
+          onClick={handleBackToHomeClick}
+        >
+          Back to Home page
+        </Button>
+      </div>
+    </div>
+  );
 };
--- src/app/ui/AppPageLoader/AppPageLoader.tsx
+++ src/app/ui/AppPageLoader/AppPageLoader.tsx
@@ -1,3 +1,16 @@
+import { Spinner } from "@nextui-org/react";
+
 export const AppPageLoader = () => {
-  return <div>loading</div>;
+  return (
+    <div className="container py-4">
+      <div className="flex justify-center">
+        <Spinner
+          size="lg"
+          color="success"
+          label="Loading..."
+          classNames={{ base: "gap-3", wrapper: "w-14 h-14", label: "text-xl" }}
+        />
+      </div>
+    </div>
+  );
 };
