f3dff373a888df58057633b61373db7d76eea5ff
Nikita Kisiura <nikitar156@gmail.com>
2024-12-06 18:55:07 -0500

    Updates

--- assets/CharacterDetails-jHmqKxgi.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-CNrlRggC.js";const r=()=>(console.log("character details page loaded"),a.jsx("div",{children:"character details page"}));export{r as CharacterDetails,r as default};
--- assets/LocationDetails-Cg_jWuk_.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CNrlRggC.js";const a=()=>(console.log("location details page loaded"),o.jsx("div",{children:"location details page"}));export{a as LocationDetails,a as default};
