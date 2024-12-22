5002bdc3fac2b79b6f4ec1d89a305098aae8eec2
Nikita Kisiura <nikitar156@gmail.com>
2024-12-22 14:47:10 -0500

    feat(shared): describe shared http types

---
+++ src/app/shared/types/http/paginated-response-dto.ts
@@ -0,0 +1,11 @@
+export interface PaginatedResponseDTO<T> {
+  readonly info: PaginationInfo;
+  readonly results: T[];
+}
+
+export interface PaginationInfo {
+  readonly count: number;
+  readonly pages: number;
+  readonly next: string | null;
+  readonly prev: string | null;
+}
