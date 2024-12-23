262205ac0c608ca7b1abe1e87455237f9e8e7488
Nikita Kisiura <nikitar156@gmail.com>
2024-12-23 13:26:16 -0500

    refactor: remove rtk-query usage

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,14 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { charactersReducer } from "@features/characters/store";
-import { rickAndMortyApi } from "@app/api";
 
 export const store = configureStore({
   reducer: {
     characters: charactersReducer,
-    [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(rickAndMortyApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
