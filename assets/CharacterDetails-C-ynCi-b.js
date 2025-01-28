258c10d91e7e3f0b46ee52ae153db08c786d67ba
Nikita Kisiura <nikitar156@gmail.com>
2025-01-27 20:46:47 -0500

    Updates

--- assets/CharacterDetails-C-ynCi-b.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./index-kd1iU-tH.js";const r=()=>(console.log("character details page loaded"),a.jsx("div",{children:"character details page"}));export{r as CharacterDetails,r as default};
--- assets/LocationDetails-CFeQPUjf.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./index-kd1iU-tH.js";const a=()=>(console.log("location details page loaded"),o.jsx("div",{children:"location details page"}));export{a as LocationDetails,a as default};
