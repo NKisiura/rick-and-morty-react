40ae9ab64ed1f2e66f8fa9c3f93ec2e019b62d40
Nikita Kisiura <nikitar156@gmail.com>
2025-02-02 17:51:51 -0500

    refactor(characters): refactor useCharacters hook

--- src/app/features/characters/hooks/useCharacters/useCharacters.ts
+++ src/app/features/characters/hooks/useCharacters/useCharacters.ts
@@ -1,20 +1,18 @@
 import { useCallback, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@app/store/hooks.ts";
-import { useConcurrentDispatchAborter } from "@app/hooks/useConcurrentDispatchAborter";
 import {
   selectCharacters,
   selectCharactersErrorMessage,
   selectCharactersLoadingStatus,
   selectCharactersPaginationInfo,
   charactersCleaned,
-  getCharactersByFilter as getCharactersByFilterAction,
+  getCharactersByFilter,
 } from "@features/characters/store";
 import { useCharactersSearchParams } from "@features/characters/hooks/useCharactersSearchParams";
 import { CharacterFilter } from "@features/characters/model";
 
 export const useCharacters = () => {
   const dispatch = useAppDispatch();
-  const concurrentDispatchAborter = useConcurrentDispatchAborter();
 
   const {
     searchParamsFilter,
@@ -26,16 +24,6 @@ export const useCharacters = () => {
   const loadingStatus = useAppSelector(selectCharactersLoadingStatus);
   const errorMessage = useAppSelector(selectCharactersErrorMessage);
   const paginationInfo = useAppSelector(selectCharactersPaginationInfo);
-
-  const getCharactersByFilter = useCallback(
-    (filter: CharacterFilter) => {
-      concurrentDispatchAborter(
-        dispatch(getCharactersByFilterAction(filter)),
-        getCharactersByFilterAction.typePrefix,
-      );
-    },
-    [concurrentDispatchAborter, dispatch],
-  );
 
   const handleFilterChange = useCallback(
     (filter: CharacterFilter) => {
@@ -52,12 +40,16 @@ export const useCharacters = () => {
   );
 
   const handleRetry = useCallback(() => {
-    getCharactersByFilter(searchParamsFilter);
-  }, [getCharactersByFilter, searchParamsFilter]);
+    void dispatch(getCharactersByFilter(searchParamsFilter));
+  }, [dispatch, searchParamsFilter]);
 
   useEffect(() => {
-    getCharactersByFilter(searchParamsFilter);
-  }, [getCharactersByFilter, searchParamsFilter]);
+    const { abort } = dispatch(getCharactersByFilter(searchParamsFilter));
+
+    return () => {
+      abort();
+    };
+  }, [dispatch, searchParamsFilter]);
 
   useEffect(() => {
     return () => {
